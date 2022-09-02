f2d520d7f990c10fc73658cf3343c43531207d68
Pravin <pravinprajapati161994@gmail.com>
2022-09-02 11:17:12 +0530

    init

--- Elightwalk/Core/registration.php
+++ Elightwalk/Core/registration.php
@@ -1,5 +1,23 @@
 <?php
-
+/**
+ * Elightwalk
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Elightwalk.com license that is
+ * available through the world-wide-web at this URL:
+ * https://store.elightwalk.com/licence
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Elightwalk
+ * @package     Elightwalk_Core
+ * @copyright   Copyright (c) Elightwalk (https://www.elightwalk.com/)
+ * @license     https://store.elightwalk.com/licence
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Elightwalk_Core',
