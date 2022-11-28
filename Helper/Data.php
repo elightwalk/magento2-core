225fa5f61096641d299c929eebeac6063b9d8d96
Ravi-Elightwalk <rmb@elightwalk.com>
2022-11-28 16:52:25 +0530

    updated helper

---
+++ Helper/Data.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Elightwalk\Core\Helper;
+
+/**
+ * Elightwalk
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Elightwalk.com license that is
+ * available through the world-wide-web at this URL:
+ * https://store.elightwalk.com/licence
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Elightwalk
+ * @package     Elightwalk_Core
+ * @copyright   Copyright (c) Elightwalk (https://www.elightwalk.com/)
+ * @license     https://store.elightwalk.com/licence
+ */
+
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Data extends \Magento\Catalog\Helper\Data
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+    }
+    /**
+     * return module status with module name
+     * @param string $path
+     * @param string $storeCode
+     * @return array
+     */
+    public function getModuleConfig($path, $storeId = null)
+    {
+        return $this->_scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
